d8458905ca403c76e6f9c16ec40784aaf049fdbd
Shahriyor <baxtiyarovich0102@gmail.com>
2025-04-16 20:50:03 +0500

    React orqali componentlar qo'shildi

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
-
+import Navbar from "./components/Navbar";
+import Main from "./pages/Main";
 
 function App() {
   return (
-    <div className="text-warning">
-      YOO wassup
+    <div>
+      <Navbar/> 
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-8 offset-lg-2">
+
+      <Main/>
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ client/src/components/TravelBook.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const TravelBook = () => {
+  return (
+    <div>
+      
+              <div class="card mb-3 mt-3">
+          <img src="https://a.travel-assets.com/findyours-php/viewfinder/images/res70/339000/339656-Downtown-Miami.jpg" class="card-img-top" alt="..."/>
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default TravelBook
